5acfa013818ce341213f4e3e32d5de27abc0fbff
ArtEze <passymas@gmail.com>
2020-03-22 03:40:52 -0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,15 +4,15 @@
 function agregar_javascript()
 {
 	var l = document.createElement("script")
-	var parte = location.href.split("/").slice(-2)[0]=="ari7a.github.io"?"":"../"
-	l.src = /*parte+*/"js/js_2.js" + "?" + r(10000000000)
+	var parte = "/ari"
+	l.src = parte + "/js/js_2.js" + "?" + r(10000000000)
 	document.head.appendChild(l)
 }
 function agregar_js_juventus()
 {
 	var l = document.createElement("script")
-	var parte = location.href.split("/").slice(-2)[0]=="ari7a.github.io"?"":"../"
-	l.src = parte+"varios/juventus.js" + "?" + r(10000000000)
+	var parte = "/ari"
+	l.src = parte + "/varios/juventus.js" + "?" + r(10000000000)
 	document.head.appendChild(l)
 }
 function js()
