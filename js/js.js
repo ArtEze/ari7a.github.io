39495accff4e0853fae234bb08014ae971287ac7
ArtEze <passymas@gmail.com>
2020-03-22 03:15:47 -0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ function agregar_javascript()
 {
 	var l = document.createElement("script")
 	var parte = location.href.split("/").slice(-2)[0]=="ari7a.github.io"?"":"../"
-	l.src = parte+"js/js_2.js" + "?" + r(10000000000)
+	l.src = /*parte+*/"js/js_2.js" + "?" + r(10000000000)
 	document.head.appendChild(l)
 }
 function agregar_js_juventus()
